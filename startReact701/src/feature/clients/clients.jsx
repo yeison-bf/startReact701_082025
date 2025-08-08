c9de088ce812fd281b1e2920522475398bea5ff8
yeison-bf <yeison.barrios.funieles@gmail.com>
2025-08-07 21:59:37 -0500

    ok

--- startReact701/src/feature/clients/clients.jsx
+++ startReact701/src/feature/clients/clients.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function Clients() {
 
@@ -6,30 +6,24 @@ export function Clients() {
     let passworsUser = "Hola1234"
 
     const [nombre, setNombre] = useState("")
-    const [apellidos, setApellidos ] = useState("")
+    const [password, setPassword ] = useState("")
 
-    const handdleAccess = (e) =>{
-        e.preventDefault()
-        console.log({nombre, apellidos})
-        if(nombre == userName && apellidos == passworsUser){
-            alert("Felicidades! lograste aceder")
-        }else{
-            alert("Vea pirobo, pailas, error de acceso")
-        }
+
+    const handleSubmit = () =>{
+        console.log({nombre, password})
     }
-
     return (
         <>
-            <h2>Formulario de registro de estudiantes</h2>
-            <form className="card p-3" onSubmit={handdleAccess}>
+            <h2>Formulario de registro deL estudiante {nombre}</h2>
+            <form className="card p-3" onSubmit={handleSubmit} >
                 <div>
                     <label>Usuario de acceso
-                        <input onChange={ (e)=> setNombre(e.target.value) } className="form-control" type="email" name="nombre" />
+                        <input onChange={ (e) => setNombre(e.target.value) } className="form-control" type="email" name="nombre" />
                     </label>
                 </div>
                 <div>
                     <label>Password
-                        <input onChange={ (e)=> setApellidos(e.target.value) } className="form-control" type="password" />
+                        <input onChange={ (e) => setPassword(e.target.value) } className="form-control" type="password" />
                     </label>
                 </div>
                 <div className="pt-4">
